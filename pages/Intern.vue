0e4fd94f48c818db93d123389923e7a917e775d4
suichanwa <augustinnegura746@gmail.com>
2023-03-26 13:23:08 -0700

    some fixes

--- pages/Intern.vue
+++ pages/Intern.vue
@@ -4,17 +4,30 @@
       <div class="card-body">
         <h5 class="card-title">Requirements for Internship Developer</h5>
         <ul>
-          <li>Experience with any programming language</li>
-          <li>Understanding of PHP and willingness to learn</li>
+        <li>Опыт работы с языком программирования PHP</li>
+          <li>Базовый синтаксис языка PHP, знание типов данных и конструкций языка</li>
+          <li>Базовые навыки вёрстки (CSS, HTML)</li>
+          <li>Опыт работы с Git (pull, commit, push, branch, checkout)</li>
+          <li>Понимание объектно-ориентированного программирования (ООП), объекты, классы, интерфейсы</li>
+          <li>Понимание архитектурного паттерна Model-View-Controller (MVC)</li>
+          <li>Базовый синтаксис языка SQL (select, update, insert, delete)</li>
+          <li>Базовое понимание работы операционной системы Linux, включая директории, пользователей</li>
+          <li>Умение работать в окружении разработки по SSH</li>
+          <li>Умение работать с инструментами разработки, включая IDE (например, PhpStorm или VSCode) и клиенты MySQL (например, DataGrip, HeidiSQL или MySQL Workbench)</li>
         </ul>
+        <Navigation :currentPage="'Intern'" />
       </div>
     </div>
   </div>
 </template>
 
-
 <script>
+import Navigation from '~/components/Navigation.vue';
+
 export default {
-  name: 'Intern'
+  name: 'Intern',
+  components: {
+    Navigation
+  }
 }
 </script>
